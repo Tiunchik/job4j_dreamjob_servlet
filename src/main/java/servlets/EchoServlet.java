01cd7c08b9e144861bb83e59a8138bae745f2b5b
Tiunchik <Senebh@gmail.com>
2020-03-19 18:02:05 +0300

    1. Создать web проект[#249907] [#00002]

---
+++ src/main/java/servlets/EchoServlet.java
@@ -0,0 +1,30 @@
+/**
+ * Package servlets for
+ *
+ * @author Maksim Tiunchik
+ */
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Class EchoServlet - 
+ *
+ * @author Maksim Tiunchik (senebh@gmail.com)
+ * @version 0.1
+ * @since 17.03.2020
+ */
+public class EchoServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(res.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}
