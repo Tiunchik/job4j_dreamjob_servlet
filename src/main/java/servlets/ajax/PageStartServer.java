5a050880f19c3c648ee53fb7a35750ad7d9507ce
Tiunchik <Senebh@gmail.com>
2020-03-27 00:06:19 +0300

    4. Ajax Jquery[#249898] [#00024]

--- src/main/java/servlets/ajax/PageStartServer.java
+++ src/main/java/servlets/ajax/PageStartServer.java
@@ -13,25 +13,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
- * Class GreetingsServlet -
+ * Class PageStartServer -
  *
  * @author Maksim Tiunchik (senebh@gmail.com)
  * @version 0.1
  * @since 26.03.2020
  */
-public class GreetingsServlet extends HttpServlet {
-    private static final Logger LOG = LogManager.getLogger(GreetingsServlet.class.getName());
+public class PageStartServer extends HttpServlet {
+    private static final Logger LOG = LogManager.getLogger(PageStartServer.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
-        writer.flush();
+        req.getRequestDispatcher("/HTML/ajaxExample.html").forward(req, resp);
     }
 }
